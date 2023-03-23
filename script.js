ee41517da9f596cb938c656f477b2af58f0306d5
justsimplycoder <justsimplycoder@gmail.com>
2023-03-23 21:07:11 +0400

    Добавление скроллинга к выбранной функции
    - Изменение модуля Menu
    - Правка стилей

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 function Menu() {
+	React.useEffect(() => {
+		document.querySelectorAll('a[href^="#"').forEach(link => {
+			link.addEventListener('click', function(e) {
+				e.preventDefault();
+				let href = this.getAttribute('href').substring(1);
+				const scrollTarget = document.getElementById('fn-' + href);
+				const topOffset = 92; // если не нужен отступ сверху
+				const elementPosition = scrollTarget.getBoundingClientRect().top;
+				const offsetPosition = elementPosition - topOffset;
+				window.scrollBy({
+						top: offsetPosition,
+						behavior: 'smooth'
+				});
+			});
+		});
+	});
+
 	return (
 		<ul className="menu">
 			{MENU[0].content.map(item => {
@@ -12,3 +29,4 @@ const toc = ReactDOM.createRoot(document.getElementById('toc'));
 toc.render(<Menu />);
 
 hljs.highlightAll();
+
