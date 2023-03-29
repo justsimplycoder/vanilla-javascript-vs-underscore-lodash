d4edbc7a7d4ab756a51c28ab74264fef1cf39979
justsimplycoder <justsimplycoder@gmail.com>
2023-03-29 21:23:52 +0400

    Добавление: Array -> pullAllWith

--- helpers/listFunction.js
+++ helpers/listFunction.js
@@ -1061,15 +1061,33 @@ pullAllBy(arr, [{ 'x': 1 }, { 'x': 3 }], 'x');
     {
       "key": "0:31",
       "name": "pullAllWith",
-      "description": "",
+      "description": "Этот метод подобен _.pullAll, за исключением того, что он принимает компаратор, который вызывается для сравнения элементов массива со значениями.",
       "lodash": `
-
+var array = [{ 'x': 1, 'y': 2 }, { 'x': 3, 'y': 4 }, { 'x': 5, 'y': 6 }];
+_.pullAllWith(array, [{ 'x': 3, 'y': 4 }], _.isEqual);
+// => [ { x: 1, y: 2 }, { x: 5, y: 6 } ]
       `,
-      "underscore": `
-
-      `,
+      "underscore": undefined,
       "vanillaJavaScript": `
+function equalObject(...objects) {
+  var arrEqual = objects.map(function (elem) {
+    return Object.entries(elem).sort(function(a, b) {
+      if (a[0] > b[0]) {
+        return 1;
+      }
+      if (a[0] < b[0]) {
+        return -1;
+      }
+      return 0;
+    });
+  });
 
+  return JSON.stringify(arrEqual[0]) == JSON.stringify(arrEqual[1]);
+}
+
+var array2 = [{ 'x': 1, 'y': 2 }, { 'x': 3, 'y': 4 }, { 'x': 5, 'y': 6 }];
+array2 = array2.filter(el => !equalObject(el, { 'x': 3, 'y': 4 }))
+// => [ { x: 1, y: 2 }, { x: 5, y: 6 } ]
       `
     },
     {
