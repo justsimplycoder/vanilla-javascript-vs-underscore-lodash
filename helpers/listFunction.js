a4b1dda15cea0715e4cc836f0fb7c2f9f8533781
justsimplycoder <justsimplycoder@gmail.com>
2023-04-02 18:40:12 +0400

    Правка: описание takeRightWhile для Vanilla JavaScript

--- helpers/listFunction.js
+++ helpers/listFunction.js
@@ -1428,7 +1428,69 @@ _.takeRightWhile(users, 'active');
       `,
       "underscore": undefined,
       "vanillaJavaScript": `
+function equalObject(...objects) {
+  var arrEqual = objects.map(function (elem) {
+    return Object.entries(elem).sort(function(a, b) {
+      if (a[0] > b[0]) {
+        return 1;
+      }
+      if (a[0] < b[0]) {
+        return -1;
+      }
+      return 0;
+    });
+  });
+  return JSON.stringify(arrEqual[0]) == JSON.stringify(arrEqual[1]);
+}
 
+function takeRightWhile(arr, condition){
+  const result = [];
+
+  if(typeof condition === 'function') {
+    for (let i = arr.length - 1; i >= 0; --i) {
+      if(condition(arr[i])) {
+        result.unshift(arr[i]);
+      } else {
+        break;
+      }
+    }
+  } else if (Array.isArray(condition)) {
+    for (let i = arr.length - 1; i >= 0; --i) {
+      if(arr[i][condition[0]] === condition[1]) {
+        result.unshift(arr[i]);
+      } else {
+        break;
+      }
+    }
+  } else if (typeof condition === 'object' && condition !== null){
+    for (let i = arr.length - 1; i >= 0; --i) {
+      if(equalObject(arr[i], condition)) {
+        result.unshift(arr[i]);
+      } else {
+        break;
+      }
+    }
+  } else if (typeof condition === 'string') {
+    for (let i = arr.length - 1; i >= 0; --i) {
+      if(!(condition in arr[i])) {
+        result.unshift(arr[i]);
+      } else {
+        break;
+      }
+    }
+  }
+
+  return result;
+}
+
+takeRightWhile(users, function(o) { return !o.active; });
+// => [ { user: 'fred', active: false }, { user: 'pebbles', active: false } ]
+takeRightWhile(users, { 'user': 'pebbles', 'active': false });
+// => [ { user: 'pebbles', active: false } ]
+takeRightWhile(users, ['active', false]);
+// => [ { user: 'fred', active: false }, { user: 'pebbles', active: false } ]
+takeRightWhile(users, 'active');
+// => []
       `
     },
     {
