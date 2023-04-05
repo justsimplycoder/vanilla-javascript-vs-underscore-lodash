07aea7289bdf8f356967ceb660b1f133d2eccc94
justsimplycoder <justsimplycoder@gmail.com>
2023-04-05 21:25:12 +0400

    Добавление: Array -> xorBy

--- helpers/listFunction.js
+++ helpers/listFunction.js
@@ -1920,15 +1920,56 @@ xor([2, 2], [2, 4]);
     {
       "key": "0:59",
       "name": "xorBy",
-      "description": "",
+      "description": "Этот метод похож на _.xor, за исключением того, что он принимает итерацию, которая вызывается для каждого элемента каждого массива, чтобы сгенерировать критерий, по которому они сравниваются.",
       "lodash": `
-
+_.xorBy([2.1, 1.2], [2.3, 3.4, 2.2], Math.floor);
+// => [ 1.2, 3.4 ]
+_.xorBy([{ 'x': 1 }], [{ 'x': 2 }, { 'x': 1 }], 'x');
+// => [ { x: 2 } ]
       `,
-      "underscore": `
-
-      `,
+      "underscore": undefined,
       "vanillaJavaScript": `
+const strategyXor = {};
 
+strategyXor.functionXor = (arr1, arr2, comparator) => {
+  const countElement = {};
+  [...arr1, ...arr2].forEach(el => {
+    countElement[comparator(el)] ? countElement[comparator(el)].push(el) : countElement[comparator(el)] = [el];
+  })
+
+  const result = [];
+  for (let prop in countElement) {
+    if (countElement[prop].length === 1) {
+      result.push(countElement[prop][0]);
+    }
+  }
+  return result;
+}
+
+strategyXor.stringXor = (arr1, arr2, comparator) => {
+  const countElement = {};
+  [...arr1, ...arr2].forEach(el => {
+    const key = el[comparator];
+    countElement[key] ? countElement[key].push(el) : countElement[key] = [el];
+  })
+
+  const result = [];
+  for (let prop in countElement) {
+    if (countElement[prop].length === 1) {
+      result.push(countElement[prop][0]);
+    }
+  }
+  return result;
+}
+
+function xorBy(arr1, arr2, comparator) {
+  return strategyXor[typeof comparator + 'Xor'](arr1, arr2, comparator);
+}
+
+xorBy([2.1, 1.2], [2.3, 3.4, 2.2], Math.floor);
+// => [ 1.2, 3.4 ]
+xorBy([{ 'x': 1 }], [{ 'x': 2 }, { 'x': 1 }], 'x');
+// => [ { x: 2 } ]
       `
     },
     {
