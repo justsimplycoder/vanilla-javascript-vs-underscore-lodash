3bca5478524dd68963e22470a8471101eef9f464
justsimplycoder <justsimplycoder@gmail.com>
2023-04-11 19:03:45 +0400

    Добавление: Collection -> orderBy

--- helpers/listFunction.js
+++ helpers/listFunction.js
@@ -2679,15 +2679,90 @@ users.map(el => el.user);
     {
       "key": "1:17",
       "name": "orderBy",
-      "description": "",
+      "description": "Этот метод похож на _.sortBy, за исключением того, что он позволяет указать порядок сортировки итераций для сортировки.",
       "lodash": `
+var users = [
+  { 'user': 'fred', 'age': 48 },
+  { 'user': 'barney', 'age': 34 },
+  { 'user': 'fred', 'age': 40 },
+  { 'user': 'barney', 'age': 36 }
+];
 
+_.orderBy(users, ['user', 'age'], ['asc', 'desc']);
+// => [
+// =>   { user: 'barney', age: 36 },
+// =>   { user: 'barney', age: 34 },
+// =>   { user: 'fred', age: 48 },
+// =>   { user: 'fred', age: 40 }
+// => ]
       `,
-      "underscore": `
-
-      `,
+      "underscore": undefined,
       "vanillaJavaScript": `
+// Вариант 1
+function compare(field, order) {
+  var len = arguments.length;
+  if (len === 0) {
+    return (a, b) => (a < b && -1) || (a > b && 1) || 0;
+  }
+  if (len === 1) {
+    switch (typeof field) {
+      case 'number':
+        return field < 0 ?
+          ((a, b) => (a < b && 1) || (a > b && -1) || 0) :
+          ((a, b) => (a < b && -1) || (a > b && 1) || 0);
+      case 'string':
+        return (a, b) => (a[field] < b[field] && -1) || (a[field] > b[field] && 1) || 0;
+    }
+  }
+  if (len === 2 && typeof order === 'number') {
+    return order < 0 ?
+      ((a, b) => (a[field] < b[field] && 1) || (a[field] > b[field] && -1) || 0) :
+      ((a, b) => (a[field] < b[field] && -1) || (a[field] > b[field] && 1) || 0);
+  }
+  var fields, orders;
+  if (typeof field === 'object') {
+    fields = Object.getOwnPropertyNames(field);
+    orders = fields.map(key => field[key]);
+    len = fields.length;
+  } else {
+    fields = new Array(len);
+    orders = new Array(len);
+    for (let i = len; i--;) {
+      fields[i] = arguments[i];
+      orders[i] = 1;
+    }
+  }
+  return (a, b) => {
+    for (let i = 0; i < len; i++) {
+      if (a[fields[i]] < b[fields[i]]) return orders[i];
+      if (a[fields[i]] > b[fields[i]]) return -orders[i];
+    }
+    return 0;
+  };
+}
 
+users.sort(compare({ user: -1, age: 1 }))
+// => [
+// =>   { user: 'barney', age: 36 },
+// =>   { user: 'barney', age: 34 },
+// =>   { user: 'fred', age: 48 },
+// =>   { user: 'fred', age: 40 }
+// => ]
+
+// Вариант 2
+users.sort(function(a, b) {
+  if (b.user > a.user) return -1;
+  if (b.user < a.user) return 1;
+  if (a.age > b.age) return -1;
+  if (a.age < b.age) return 1;
+  return 0;
+})
+// => [
+// =>   { user: 'barney', age: 36 },
+// =>   { user: 'barney', age: 34 },
+// =>   { user: 'fred', age: 48 },
+// =>   { user: 'fred', age: 40 }
+// => ]
       `
     },
     {
