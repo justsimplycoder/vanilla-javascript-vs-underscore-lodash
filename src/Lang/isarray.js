4e96b01fa4b537edff71cfac2177478def482ecb
justsimplycoder <justsimplycoder@gmail.com>
2023-05-19 21:22:04 +0400

    Правка: тест для функции Math -> isArray

--- src/Lang/isarray.js
+++ src/Lang/isarray.js
@@ -1,22 +1,8 @@
-const und = require('underscore');
-const lod = require('lodash');
-
 /**
  * isArray - Проверяет, классифицируется ли значение как объект Array.
  */
-
-// underscore
-console.log("und.isArray([1, 2, 3])", und.isArray([1, 2, 3]));
-console.log("und.isArray('abc')", und.isArray('abc'));
-
-// lodash
-console.log("lod.isArray([1, 2, 3])", lod.isArray([1, 2, 3]));
-console.log("lod.isArray('abc')", lod.isArray('abc'));
-
-// es6
 function isArray(value) {
   return Array.isArray(value);
 }
 
-console.log("isArray([1, 2, 3])", isArray([1, 2, 3]));
-console.log("isArray('abc')", isArray('abc'));
+export default isArray;
--- test/Lang/isarray.test.js
+++ test/Lang/isarray.test.js
@@ -1,3 +1,29 @@
+import isArray from '../../src/Lang/isArray.js'
+import lod_isArray from '../../node_modules/lodash-es/isArray.js';
+import und_isArray from '../../node_modules/underscore/modules/isarray.js';
+const lod = {};
+lod.isArray = lod_isArray;
+const und = {};
+und.isArray = und_isArray;
+
+
+// Примеры использования
+console.log('---------------underscore---------------');
+console.log("_.isArray([1, 2, 3])", und.isArray([1, 2, 3]));
+console.log("_.isArray('abc')", und.isArray('abc'));
+
+console.log('-----------------lodash-----------------');
+console.log("_.isArray([1, 2, 3])", lod.isArray([1, 2, 3]));
+console.log("_.isArray('abc')", lod.isArray('abc'));
+
+console.log('-------------------ES-------------------');
+console.log("isArray([1, 2, 3])", isArray([1, 2, 3]));
+console.log("isArray('abc')", isArray('abc'));
+
+
+// Тесты
+let assert = chai.assert;
+
 describe('isArray', function() {
 	it('должен возвращать `true` для массивов', function() {
 		assert.strictEqual(isArray([1, 2, 3]), true);
@@ -16,3 +42,5 @@ describe('isArray', function() {
 		assert.strictEqual(isArray(Symbol('x')), false);
 	});
 });
+
+mocha.run();
