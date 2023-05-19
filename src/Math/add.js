3156113d95785c3e464b8fb4adef844432efd904
justsimplycoder <justsimplycoder@gmail.com>
2023-05-19 17:38:29 +0400

    Правка: тест для функции Math -> add

--- src/Math/add.js
+++ src/Math/add.js
@@ -1,17 +1,10 @@
-const und = require('underscore');
-const lod = require('lodash');
-
 /**
  * add - складывание 2-х чисел
  */
 
-// lodash
-console.log("lod.add(4, 6)", lod.add(4, 6));
-console.log("lod.add('4', '6')", lod.add('4', '6'));
-
 // es6
 function add(a, b) {
-  return a + b;
+	return a + b;
 }
-console.log("add(4, 6)", add(4, 6));
-console.log("add('4', '6')", add('4', '6'));
+
+export default add;
--- test/Math/add.test.js
+++ test/Math/add.test.js
@@ -1,3 +1,20 @@
+import add from '../../src/Math/add.js'
+import lod_add from '../../node_modules/lodash-es/add.js';
+const lod = {};
+lod.add = lod_add;
+
+// Примеры использования
+console.log('-----------------lodash-----------------');
+console.log("_.add(4, 6)", lod.add(4, 6));
+console.log("_.add('4', '6')", lod.add('4', '6'));
+
+console.log('-------------------ES-------------------');
+console.log("add(4, 6)", add(4, 6));
+console.log("add('4', '6')", add('4', '6'));
+
+// Тесты
+let assert = chai.assert;
+
 describe('add', function() {
 	it('нужно добавить два числа', function() {
 		assert.strictEqual(add(6, 4), 10);
@@ -10,3 +27,5 @@ describe('add', function() {
 		assert.strictEqual(add('x', 'y'), 'xy');
 	});
 });
+
+mocha.run();
