0c10b152023357bfc3446b9b9d27ab432a558af1
justsimplycoder <justsimplycoder@gmail.com>
2023-05-19 22:22:10 +0400

    Правка: тест для функции Math -> isArrayBuffer

--- test/Lang/isarraybuffer.test.js
+++ test/Lang/isarraybuffer.test.js
@@ -1,3 +1,21 @@
+import isArrayBuffer from '../../src/Lang/isArrayBuffer.js'
+import lod_isArrayBuffer from '../../node_modules/lodash-es/isArrayBuffer.js';
+const lod = {};
+lod.isArrayBuffer = lod_isArrayBuffer;
+
+// Примеры использования
+console.log('-----------------lodash-----------------');
+console.log("_.isArrayBuffer(new ArrayBuffer(2))", lod.isArrayBuffer(new ArrayBuffer(2)));
+console.log("_.isArrayBuffer(new Array(2))", lod.isArrayBuffer(new Array(2)));
+
+console.log('-------------------ES-------------------');
+console.log("isArrayBuffer(new ArrayBuffer(2))", isArrayBuffer(new ArrayBuffer(2)));
+console.log("isArrayBuffer(new Array(2))", isArrayBuffer(new Array(2)));
+
+
+// Тесты
+let assert = chai.assert;
+
 describe('isArrayBuffer', function() {
 	it('должен возвращать `true` для ArrayBuffer', function() {
 		assert.strictEqual(isArrayBuffer(new ArrayBuffer(2)), true);
@@ -17,3 +35,5 @@ describe('isArrayBuffer', function() {
 		assert.strictEqual(isArrayBuffer(Symbol('x')), false);
 	});
 });
+
+mocha.run();
