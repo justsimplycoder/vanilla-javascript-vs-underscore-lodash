71bfe9ce39944d29cb758d88508fcaaafb7205f4
justsimplycoder <justsimplycoder@gmail.com>
2023-05-22 22:08:04 +0400

    Добавление: test -> Lang -> isFinite

--- src/Lang/isfinite.js
+++ src/Lang/isfinite.js
@@ -1,28 +1,10 @@
-const und = require('underscore');
-const lod = require('lodash');
-
 /**
  * isFinite - Проверяет, является ли значение конечным примитивным числом.
  */
 
-// underscore
-console.log("und.isFinite(3)", und.isFinite(3));
-console.log("und.isFinite(Number.MIN_VALUE)", und.isFinite(Number.MIN_VALUE));
-console.log("und.isFinite(Infinity)", und.isFinite(Infinity));
-console.log("und.isFinite('3')", und.isFinite('3'));
-
-// lodash
-console.log("lod.isFinite(3)", lod.isFinite(3));
-console.log("lod.isFinite(Number.MIN_VALUE)", lod.isFinite(Number.MIN_VALUE));
-console.log("lod.isFinite(Infinity)", lod.isFinite(Infinity));
-console.log("lod.isFinite('3')", lod.isFinite('3'));
-
 // es6
 function isFiniteValue(value) {
-	return isFinite(value) && typeof value !== 'string';
+	return Number.isFinite(value);
 }
 
-console.log("isFiniteValue(3)", isFiniteValue(3));
-console.log("isFiniteValue(Number.MIN_VALUE)", isFiniteValue(Number.MIN_VALUE));
-console.log("isFiniteValue(Infinity)", isFiniteValue(Infinity));
-console.log("isFiniteValue('3')", isFiniteValue('3'));
+export default isFiniteValue;
---
+++ test/Lang/isfinite.test.js
@@ -0,0 +1,70 @@
+import isFiniteValue from '../../src/Lang/isFinite.js'
+import lod_isFinite from '../../node_modules/lodash-es/isFinite.js';
+const lod = {};
+lod.isFinite = lod_isFinite;
+import und_isFinite from '../../node_modules/underscore/modules/isFinite.js';
+const und = {};
+und.isFinite = und_isFinite;
+
+// Примеры использования
+console.log('---------------underscore---------------');
+console.log("und.isFinite(3)", und.isFinite(3));
+console.log("und.isFinite(Number.MIN_VALUE)", und.isFinite(Number.MIN_VALUE));
+console.log("und.isFinite(Infinity)", und.isFinite(Infinity));
+console.log("und.isFinite('3')", und.isFinite('3'));
+
+console.log('-----------------lodash-----------------');
+console.log("lod.isFinite(3)", lod.isFinite(3));
+console.log("lod.isFinite(Number.MIN_VALUE)", lod.isFinite(Number.MIN_VALUE));
+console.log("lod.isFinite(Infinity)", lod.isFinite(Infinity));
+console.log("lod.isFinite('3')", lod.isFinite('3'));
+
+console.log('-------------------ES-------------------');
+console.log("isFiniteValue(3)", isFiniteValue(3));
+console.log("isFiniteValue(Number.MIN_VALUE)", isFiniteValue(Number.MIN_VALUE));
+console.log("isFiniteValue(Infinity)", isFiniteValue(Infinity));
+console.log("isFiniteValue('3')", isFiniteValue('3'));
+
+// Тесты
+let assert = chai.assert;
+
+describe('isFinite', function() {
+	it('должен возвращать `true` для конечных значений', function() {
+		const values = [0, 1, 3.14, -1, Number(1)];
+		for (let elem of values) {
+			assert.strictEqual(isFiniteValue(elem), true);
+		}
+	});
+
+	it('должен возвращать `false` для неконечных значений', function() {
+		const values = [NaN, Infinity, -Infinity, Object(1)];
+		for (let elem of values) {
+			assert.strictEqual(isFiniteValue(elem), false);
+		}
+	});
+
+	it('должен возвращать `false` для числовых строковых значений', function() {
+		const values = ['0', '1', '3.14'];
+		for (let elem of values) {
+			assert.strictEqual(isFiniteValue(elem), false);
+		}
+	});
+
+	it('должен возвращать `false` для нечисловых значений', function() {
+		assert.strictEqual(isFiniteValue([1, 2, 3]), false);
+		assert.strictEqual(isFiniteValue(true), false);
+		assert.strictEqual(isFiniteValue(new Date), false);
+		assert.strictEqual(isFiniteValue(new Error), false);
+		assert.strictEqual(isFiniteValue({ 'a': 1 }), false);
+		assert.strictEqual(isFiniteValue(/x/), false);
+		assert.strictEqual(isFiniteValue('a'), false);
+		assert.strictEqual(isFiniteValue(''), false);
+		assert.strictEqual(isFiniteValue(' '), false);
+		assert.strictEqual(isFiniteValue(null), false);
+		assert.strictEqual(isFiniteValue(undefined), false);
+		let symbol = Symbol();
+		assert.strictEqual(isFiniteValue(symbol), false);
+	});
+});
+
+mocha.run();
