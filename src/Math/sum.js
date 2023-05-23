32657c926adbbdae035723925ce83e011e628f2f
justsimplycoder <justsimplycoder@gmail.com>
2023-05-23 11:08:59 +0400

    Добавление: test -> Math -> sum

--- src/Math/sum.js
+++ src/Math/sum.js
@@ -1,12 +1,17 @@
-const und = require('underscore');
-const lod = require('lodash');
-
 /**
  * sum - сумма всех элементов массива
  */
 
-// lodash
-console.log("lod.sum([4, 2, 8, 6])", lod.sum([4, 2, 8, 6]));
-
 // es6
-console.log("[4, 2, 8, 6].reduce((sum, current) => sum + current)", [4, 2, 8, 6].reduce((sum, current) => sum + current));
+function sum(arr) {
+	let start = 0;
+	if(typeof arr[0] === 'string') {
+		start = '';
+	}
+	return arr.reduce((sum, current) => {
+		if(current == undefined) return sum;
+		return sum + current
+	}, start);
+}
+
+export default sum;
---
+++ test/Math/sum.test.js
@@ -0,0 +1,38 @@
+import sum from '../../src/Math/sum.js'
+import lod_sum from '../../node_modules/lodash-es/sum.js';
+const lod = {};
+lod.sum = lod_sum;
+
+// Примеры использования
+console.log('-----------------lodash-----------------');
+console.log("lod.sum([4, 2, 8, 6])", lod.sum([4, 2, 8, 6]));
+
+console.log('-------------------ES-------------------');
+console.log("sum([4, 2, 8, 6])", sum([4, 2, 8, 6]));
+
+// Тесты
+let assert = chai.assert;
+
+describe('sum', function() {
+	it('метод sum должен возвращать сумму массива чисел', function() {
+		assert.strictEqual(sum([1, 2, 3]), 6);
+	});
+
+	it('метод sum должен возвращать `0` при передаче пустых значений `массива`', function() {
+		assert.strictEqual(sum([]), 0);
+	});
+
+	it('метод sum следует пропускать `неопределенные` значения', function() {
+		assert.strictEqual(sum([1, undefined]), 1);
+	});
+
+	it('метод sum не следует пропускать значения `NaN`', function() {
+		assert.deepStrictEqual(sum([1, NaN]), NaN);
+	});
+
+	it('метод sum не следует приводить значения к числам', function() {
+		assert.deepStrictEqual(sum(['1', '2']), '12');
+	});
+});
+
+mocha.run();
