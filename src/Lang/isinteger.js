4321ac9b875230253f519d03ba200a3cef04b647
justsimplycoder <justsimplycoder@gmail.com>
2023-06-06 21:13:47 +0400

    Добавление: test -> Lang -> isInteger

--- src/Lang/isinteger.js
+++ src/Lang/isinteger.js
@@ -1,23 +1,9 @@
-const und = require('underscore');
-const lod = require('lodash');
-
 /**
  * isInteger - Проверяет, является ли значение целым числом.
  */
 
-// lodash
-console.log("lod.isInteger(3)", lod.isInteger(3));
-console.log("lod.isInteger(Number.MIN_VALUE)", lod.isInteger(Number.MIN_VALUE));
-console.log("lod.isInteger(Infinity)", lod.isInteger(Infinity));
-console.log("lod.isInteger('3')", lod.isInteger('3'));
+function isInteger(value) {
+  return Number.isInteger(value);
+}
 
-// es6
-console.log("parseInt(3) === 3", parseInt(3) === 3);
-console.log("parseInt(Number.MIN_VALUE) === Number.MIN_VALUE", parseInt(Number.MIN_VALUE) === Number.MIN_VALUE);
-console.log("parseInt(Infinity) === Infinity", parseInt(Infinity) === Infinity);
-console.log("parseInt('3') === '3'", parseInt('3') === '3');
-
-console.log("Number.isInteger(3)", Number.isInteger(3));
-console.log("Number.isInteger(Number.MIN_VALUE)", Number.isInteger(Number.MIN_VALUE));
-console.log("Number.isInteger(Infinity)", Number.isInteger(Infinity));
-console.log("Number.isInteger('3')", Number.isInteger('3'));
+export default isInteger;
---
+++ test/Lang/isinteger.test.js
@@ -0,0 +1,48 @@
+import isInteger from '../../src/Lang/isInteger.js'
+import lod_isInteger from '../../node_modules/lodash-es/isInteger.js';
+const lod = {};
+lod.isInteger = lod_isInteger;
+
+// Примеры использования
+console.log('-----------------lodash-----------------');
+console.log("_.isInteger(3)", lod.isInteger(3));
+console.log("_.isInteger(Number.MIN_VALUE)", lod.isInteger(Number.MIN_VALUE));
+console.log("_.isInteger(Infinity)", lod.isInteger(Infinity));
+console.log("_.isInteger('3')", lod.isInteger('3'));
+
+console.log('-------------------ES-------------------');
+console.log("isInteger(3)", isInteger(3));
+console.log("isInteger(Number.MIN_VALUE)", isInteger(Number.MIN_VALUE));
+console.log("isInteger(Infinity)", isInteger(Infinity));
+console.log("isInteger('3')", isInteger('3'));
+
+// Тесты
+let assert = chai.assert;
+
+describe('isInteger', function() {
+	it('должен возвращать `true` для целочисленных значений', function() {
+		assert.strictEqual(isInteger(1), true);
+		assert.strictEqual(isInteger(Number.MAX_VALUE), true);
+	});
+	it('должен возвращать `false` для не целых чисел', function() {
+		assert.strictEqual(isInteger(Number.MIN_VALUE), false);
+		assert.strictEqual(isInteger(NaN), false);
+		assert.strictEqual(isInteger(1.2), false);
+		assert.strictEqual(isInteger(Object(1)), false);
+		assert.strictEqual(isInteger(Infinity), false);
+		assert.strictEqual(isInteger(-Infinity), false);
+	});
+	it('должен возвращать `false` для нечисловых значений', function() {
+		assert.strictEqual(isInteger([1, 2, 3]), false);
+		assert.strictEqual(isInteger(true), false);
+		assert.strictEqual(isInteger(new Date), false);
+		assert.strictEqual(isInteger(new Error), false);
+		assert.strictEqual(isInteger({ 'a': 1 }), false);
+		assert.strictEqual(isInteger(/x/), false);
+		assert.strictEqual(isInteger('a'), false);
+		assert.strictEqual(isInteger('3'), false);
+
+	});
+});
+
+mocha.run();
