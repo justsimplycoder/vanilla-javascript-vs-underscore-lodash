54bd0b451fdf70e502c96e9d1d12510f4dedead7
justsimplycoder <justsimplycoder@gmail.com>
2023-06-21 20:41:38 +0400

    Добавление: test -> Lang -> isNumber

--- src/Lang/isnumber.js
+++ src/Lang/isnumber.js
@@ -1,31 +1,12 @@
-const und = require('underscore');
-const lod = require('lodash');
-
 /**
  * isNumber - Проверяет, классифицируется ли значение как числовой примитив или объект.
  */
 
-// underscore
-console.log("und.isNumber(3)", und.isNumber(3));
-console.log("und.isNumber(Number.MIN_VALUE)", und.isNumber(Number.MIN_VALUE));
-console.log("und.isNumber(Infinity)", und.isNumber(Infinity));
-console.log("und.isNumber('3')", und.isNumber('3'));
-console.log("lod.isNumber({})", lod.isNumber({}));
-
-// lodash
-console.log("lod.isNumber(3)", lod.isNumber(3));
-console.log("lod.isNumber(Number.MIN_VALUE)", lod.isNumber(Number.MIN_VALUE));
-console.log("lod.isNumber(Infinity)", lod.isNumber(Infinity));
-console.log("lod.isNumber('3')", lod.isNumber('3'));
-console.log("lod.isNumber({})", lod.isNumber({}));
-
-// es6
 function isNumber(n){
-	return Number(n) === n;
+  // console.log(Object(0) === 0);
+	// return Number(n) === n;
+  // console.log();
+  return Object.prototype.toString.call(n) === '[object Number]';
 }
 
-console.log("isNumber(3)", isNumber(3));
-console.log("isNumber(Number.MIN_VALUE)", isNumber(Number.MIN_VALUE));
-console.log("isNumber(Infinity)", isNumber(Infinity));
-console.log("isNumber('3')", isNumber('3'));
-console.log("isNumber({})", isNumber({}));
+export default isNumber;
---
+++ test/Lang/isnumber.test.js
@@ -0,0 +1,50 @@
+import isNumber from '../../src/Lang/isNumber.js'
+import lod_isNumber from '../../node_modules/lodash-es/isNumber.js';
+const lod = {};
+lod.isNumber = lod_isNumber;
+import und_isNumber from '../../node_modules/underscore/modules/isNumber.js';
+const und = {};
+und.isNumber = und_isNumber;
+
+// Примеры использования
+console.log('---------------underscore---------------');
+console.log("und.isNumber(3)", und.isNumber(3));
+console.log("und.isNumber('3')", und.isNumber('3'));
+
+console.log('-----------------lodash-----------------');
+console.log("lod.isNumber(3)", lod.isNumber(3));
+console.log("lod.isNumber('3')", lod.isNumber('3'));
+
+console.log('-------------------ES-------------------');
+console.log("isNumber(3)", isNumber(3));
+console.log("isNumber('3')", isNumber('3'));
+
+// Тесты
+let assert = chai.assert;
+
+describe('isNumber', function() {
+	it('должен возвращать `true` для чисел', function() {
+		assert.strictEqual(isNumber(0), true);
+		assert.strictEqual(isNumber(Infinity), true);
+		assert.strictEqual(isNumber(Object(0)), true);
+		assert.strictEqual(isNumber(Number(0)), true);
+		assert.strictEqual(isNumber(Number.MIN_VALUE), true);
+		assert.strictEqual(isNumber(NaN), true);
+	});
+
+	it('должен возвращать `false` для нечисел', function() {
+		assert.strictEqual(isNumber(null), false);
+		assert.strictEqual(isNumber([1, 2, 3]), false);
+		assert.strictEqual(isNumber(new Date), false);
+		assert.strictEqual(isNumber(new Error), false);
+		assert.strictEqual(isNumber(Array.prototype.slice), false);
+		assert.strictEqual(isNumber({ 'a': 1 }), false);
+		assert.strictEqual(isNumber('1'), false);
+		assert.strictEqual(isNumber(/x/), false);
+		assert.strictEqual(isNumber('a'), false);
+		let symbol = Symbol();
+		assert.strictEqual(isNumber(symbol), false);
+	});
+});
+
+mocha.run();
