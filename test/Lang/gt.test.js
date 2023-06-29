6df6862c7a05113d9481c5dcda8200efe03a3210
justsimplycoder <justsimplycoder@gmail.com>
2023-06-29 13:19:44 +0400

    Добавление: test -> Lang -> gt

---
+++ test/Lang/gt.test.js
@@ -0,0 +1,37 @@
+import gt from '../../src/Lang/gt.js'
+import lod_gt from '../../node_modules/lodash-es/gt.js';
+const lod = {};
+lod.gt = lod_gt;
+
+// Примеры использования
+var object = { 'a': 1 };
+var other = { 'a': 1 };
+
+console.log('-----------------lodash-----------------');
+console.log("lod.gt(1, 3)", lod.gt(1, 3));
+console.log("lod.gt(3, 3)", lod.gt(3, 3));
+console.log("lod.gt(3, 1)", lod.gt(3, 1));
+
+console.log('-------------------ES-------------------');
+console.log("gt(1, 3)", gt(1, 3));
+console.log("gt(3, 3)", gt(3, 3));
+console.log("gt(3, 1)", gt(3, 1));
+
+// Тесты
+let assert = chai.assert;
+
+describe('gt', function() {
+	it('должен возвращать `true`, если `value` `other`', function() {
+		assert.strictEqual(gt(3, 1), true);
+		assert.strictEqual(gt('def', 'abc'), true);
+	});
+
+	it('должен возвращать `false`, если `value` равно `other`', function () {
+		assert.strictEqual(gt(1, 3), false);
+		assert.strictEqual(gt(3, 3), false);
+		assert.strictEqual(gt('abc', 'def'), false);
+		assert.strictEqual(gt('def', 'def'), false);
+	})
+});
+
+mocha.run();
