7e671a6126c10333ee81088c7efc417d82543aa1
justsimplycoder <justsimplycoder@gmail.com>
2023-07-04 21:24:22 +0400

    Добавление: test -> Lang -> lt

--- src/Lang/lt.js
+++ src/Lang/lt.js
@@ -1,20 +1,9 @@
-const und = require('underscore');
-const lod = require('lodash');
-
 /**
  * lt - проверяет является ли первый параметр меньше второго
 */
 
-// lodash
-console.log("lod.lt(1, 3)", lod.lt(1, 3));
-console.log("lod.lt(3, 3)", lod.lt(3, 3));
-console.log("lod.lt(3, 1)", lod.lt(3, 1));
-
-// es6
 function lt(el1, el2) {
 	return el1 < el2;
 }
 
-console.log("lt(1, 3)", lt(1, 3));
-console.log("lt(3, 3)", lt(3, 3));
-console.log("lt(3, 1)", lt(3, 1));
+export default lt;
---
+++ test/Lang/lt.test.js
@@ -0,0 +1,34 @@
+import lt from '../../src/Lang/lt.js'
+import lod_lt from '../../node_modules/lodash-es/lt.js';
+const lod = {};
+lod.lt = lod_lt;
+
+// Примеры использования
+console.log('-----------------lodash-----------------');
+console.log("lod.lt(1, 3)", lod.lt(1, 3));
+console.log("lod.lt(3, 3)", lod.lt(3, 3));
+console.log("lod.lt(3, 1)", lod.lt(3, 1));
+
+console.log('-------------------ES-------------------');
+console.log("lt(1, 3)", lt(1, 3));
+console.log("lt(3, 3)", lt(3, 3));
+console.log("lt(3, 1)", lt(3, 1));
+
+// Тесты
+let assert = chai.assert;
+
+describe('lt', function() {
+	it('должен возвращать `true`, если `value` < `other`', function() {
+		assert.strictEqual(lt(1, 3), true);
+    assert.strictEqual(lt('abc', 'def'), true);
+	});
+
+	it('должен возвращать `false`, если `value` >= `other`', function () {
+    assert.strictEqual(lt(3, 1), false);
+    assert.strictEqual(lt(3, 3), false);
+    assert.strictEqual(lt('def', 'abc'), false);
+    assert.strictEqual(lt('def', 'def'), false);
+	})
+});
+
+mocha.run();
