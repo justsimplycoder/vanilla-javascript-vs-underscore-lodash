26298986d368670cbcc5b133b388423e6bd1a29b
justsimplycoder <justsimplycoder@gmail.com>
2023-07-07 21:04:41 +0400

    Добавление: test -> Lang -> isWeakSet

--- src/Lang/isweakset.js
+++ src/Lang/isweakset.js
@@ -1,23 +1,10 @@
-const und = require('underscore');
-const lod = require('lodash');
-
 /**
  * isWeakSet - проверяет является ли выражение WeakSet
 */
 
-// underscore
-console.log("und.isWeakSet(new WeakSet)", und.isWeakSet(new WeakSet));
-console.log("und.isWeakSet(new Set)", und.isWeakSet(new Set));
-
-// lodash
-console.log("lod.isWeakSet(new WeakSet)", lod.isWeakSet(new WeakSet));
-console.log("lod.isWeakSet(new Set)", lod.isWeakSet(new Set));
-
-// es6
 function isWeakSet(value) {
 	if(Object.prototype.toString.call(value) === '[object WeakSet]') return true;
 	return false;
 }
 
-console.log("isWeakSet(new WeakSet)", isWeakSet(new WeakSet));
-console.log("isWeakSet(new Set)", isWeakSet(new Set));
+export default isWeakSet;
---
+++ test/Lang/isweakset.test.js
@@ -0,0 +1,48 @@
+import isWeakSet from '../../src/Lang/isWeakSet.js'
+import lod_isWeakSet from '../../node_modules/lodash-es/isWeakSet.js';
+const lod = {};
+lod.isWeakSet = lod_isWeakSet;
+import und_isWeakSet from '../../node_modules/underscore/modules/isWeakSet.js';
+const und = {};
+und.isWeakSet = und_isWeakSet;
+
+// Примеры использования
+console.log('---------------underscore---------------');
+console.log("und.isWeakSet(new WeakSet)", und.isWeakSet(new WeakSet));
+console.log("und.isWeakSet(new Set)", und.isWeakSet(new Set));
+
+console.log('-----------------lodash-----------------');
+console.log("lod.isWeakSet(new WeakSet)", lod.isWeakSet(new WeakSet));
+console.log("lod.isWeakSet(new Set)", lod.isWeakSet(new Set));
+
+console.log('-------------------ES-------------------');
+console.log("isWeakSet(new WeakSet)", isWeakSet(new WeakSet));
+console.log("isWeakSet(new Set)", isWeakSet(new Set));
+
+// Тесты
+let assert = chai.assert;
+
+describe('isWeakSet', function() {
+	it('должен возвращать `true` для WeakSet', function() {
+		if (WeakSet) {
+			assert.strictEqual(isWeakSet(new WeakSet()), true);
+		}
+	});
+
+	it('должен возвращать `false` для не WeakSet', function() {
+		assert.strictEqual(isWeakSet(null), false);
+		assert.strictEqual(isWeakSet([1, 2, 3]), false);
+		assert.strictEqual(isWeakSet(new Date), false);
+		assert.strictEqual(isWeakSet(new Error), false);
+		assert.strictEqual(isWeakSet(Array.prototype.slice), false);
+		assert.strictEqual(isWeakSet({ 'a': 1 }), false);
+		assert.strictEqual(isWeakSet(1), false);
+		assert.strictEqual(isWeakSet(/x/), false);
+		assert.strictEqual(isWeakSet('a'), false);
+		let symbol = Symbol();
+		assert.strictEqual(isWeakSet(symbol), false);
+		assert.strictEqual(isWeakSet(new Set([1, 2, 3])), false);
+	});
+});
+
+mocha.run();
