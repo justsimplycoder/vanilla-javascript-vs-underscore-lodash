55aff43bff06b3421e55e46e126005bbbf63fadf
justsimplycoder <justsimplycoder@gmail.com>
2023-07-10 18:48:06 +0400

    Добавление: test -> Lang -> toString

--- src/Lang/tostring.js
+++ src/Lang/tostring.js
@@ -1,44 +1,22 @@
-const und = require('underscore');
-const lod = require('lodash');
-
 /**
  * toString - Преобразует значение в строку.
  */
 
-// lodash
-console.log("lod.toString(null)", lod.toString(null));
-console.log("lod.toString(undefined)", lod.toString(undefined));
-console.log("lod.toString([1, 2, 3])", lod.toString([1, 2, 3]));
-console.log("lod.toString(-0)", lod.toString(-0));
-console.log("lod.toString(0)", lod.toString(0));
-console.log("lod.toString()", lod.toString());
-console.log("lod.toString('')", lod.toString(''));
-console.log("lod.toString('text')", lod.toString('text'));
-
-// es6
-/**
- * Функция проверки, является ли значение -0
- * @param  {any}  x - проверяемое значение
- * @return {Boolean}
- */
-// function isNegativeZero(x) {
-// 	return x === 0 && (1 / x) === -Infinity
-// }
-
-function toString(value) {
+function convertString(value) {
+	// Пустое значение
 	if(value == null) {
 		return '';
 	}
-	// if(isNegativeZero(value)) return '-0';
+	// Преобразование
+	if(typeof value === 'object' && value instanceof Number) value = value.valueOf();
+	// Преобразование -0 в строку '-0'
 	if(Object.is(-0, value)) return '-0';
 	return (value).toString();
 }
 
-console.log("toString(null)", toString(null));
-console.log("toString(undefined)", toString(undefined));
-console.log("toString([1, 2, 3])", toString([1, 2, 3]));
-console.log("toString(-0)", toString(-0));
-console.log("toString(0)", toString(0));
-console.log("toString()", toString());
-console.log("toString('')", toString(''));
-console.log("toString('text')", toString('text'));
+function toString(value) {
+	if(Array.isArray(value)) value = value.map(el => convertString(el));
+	return convertString(value);
+}
+
+export default toString;
---
+++ test/Lang/tostring.test.js
@@ -0,0 +1,50 @@
+import toString from '../../src/Lang/toString.js'
+import lod_toString from '../../node_modules/lodash-es/toString.js';
+const lod = {};
+lod.toString = lod_toString;
+
+// Примеры использования
+console.log('-----------------lodash-----------------');
+console.log("lod.toString([1, 2, 3])", lod.toString([1, 2, 3]));
+console.log("lod.toString(-0)", lod.toString(-0));
+console.log("lod.toString(0)", lod.toString(0));
+console.log("lod.toString('text')", lod.toString('text'));
+
+console.log('-------------------ES-------------------');
+console.log("toString([1, 2, 3])", toString([1, 2, 3]));
+console.log("toString(-0)", toString(-0));
+console.log("toString(0)", toString(0));
+console.log("toString('text')", toString('text'));
+
+// Тесты
+let assert = chai.assert;
+
+describe('toString', function() {
+	it('должен обрабатывать нулевые значения как пустые строки', function() {
+		assert.strictEqual(toString(null), '');
+		assert.strictEqual(toString(undefined), '');
+		assert.strictEqual(toString(), '');
+	});
+
+	it('должен сохранять знак `0`', function() {
+		assert.strictEqual(toString(-0), '-0');
+		assert.strictEqual(toString(Object(-0)), '-0');
+		assert.strictEqual(toString(0), '0');
+		assert.strictEqual(toString(Object(0)), '0');
+	});
+
+	it('должен сохранять знак `0` в массиве', function() {
+		const values = [-0, Object(-0), 0, Object(0)];
+		assert.strictEqual(toString(values), '-0,-0,0,0');
+	});
+
+	it('должен обрабатывать символы', function() {
+		assert.strictEqual(toString(Symbol('x')), 'Symbol(x)');
+	});
+
+	it('должен обрабатывать массив символов', function() {
+		assert.strictEqual(toString([Symbol('x'), Symbol('y')]), 'Symbol(x),Symbol(y)');
+	});
+});
+
+mocha.run();
